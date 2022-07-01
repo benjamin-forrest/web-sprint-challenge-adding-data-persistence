e41fe1ff841271491439f25473da05ae165e46d4
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-06-30 19:34:25 -0700

    index.js and model.js

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,38 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+async function getAll() {
+    const result = await db('projects')
+    const adjustedResult = result.map(project => {
+        project.project_name,
+        project.project_description,
+        project.project_completed = project.project_completed ? true : false
+        return project
+    })
+    return adjustedResult
+}
+
+async function getById(id) {
+
+    const project = await db('projects').where('project_id', id).first()
+
+    const adjustedProject = {
+        project_name: project.project_name,
+        project_description: project.project_description,
+        project_completed: project.project_completed ? true : false
+        
+    }
+    return adjustedProject
+}
+
+async function create(project) {
+    const [id] = await db('projects').insert(project)
+    return getById(id);
+}
+
+
+module.exports = {
+    getAll,
+    create,
+    
+}
